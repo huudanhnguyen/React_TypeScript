1f07d4928b1c13e2409775fbf2ad3a80b3afb1eb
Danh <huudanh2001.hd@gmail.com>
2025-10-26 20:35:42 +0700

    format axios

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,6 @@
+import axios from "services/axios.customize";
+
+export const LoginAPI = (username: string, password: string) => {
+  const urlBackend = "/api/v1/auth/login";
+  return axios.post<IBackendRes<ILogin>>(urlBackend, { username, password });
+};
