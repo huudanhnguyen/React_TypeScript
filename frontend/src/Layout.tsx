b3223233612fb43800679b9fd8464071916b31ca
Danh <huudanh2001.hd@gmail.com>
2025-10-27 08:23:38 +0700

    handle F5 reload get user

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -2,12 +2,17 @@ import { Outlet } from "react-router-dom"
 import AppHeader from "./components/layouts/app.header"
 import { useEffect } from "react"
 import { fetchAccountAPI } from "./services/api"
+import { useCurrentApp } from "./components/context/app.context"
 
 function Layout() {
+  const{setUser,setIsAppLoading}=useCurrentApp();
   useEffect(()=>{
     const fetchAccount= async()=>{
       const res=await fetchAccountAPI();
-      console.log(res)
+      if(res.data){
+        setUser(res.data.user)
+      }
+      setIsAppLoading(false)
     }
     fetchAccount();
   },[])
--- frontend/src/components/context/app.context.tsx
+++ frontend/src/components/context/app.context.tsx
@@ -1,9 +1,11 @@
-import { createContext, useState ,useContext} from "react";
+import { createContext, useState, useContext } from "react";
 interface IAppContext {
   isAuthenticated: boolean;
   setIsAuthenticated: (v: boolean) => void;
   setUser: (v: IUser) => void;
   user: IUser | null;
+  isAppLoading: boolean;
+  setIsAppLoading: (v: boolean) => void;
 }
 
 const CurrentAppContext = createContext<IAppContext | null>(null);
@@ -15,13 +17,16 @@ type TProps = {
 export const AppProvider = (props: TProps) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   const [user, setUser] = useState<IUser | null>(null);
+  const [isAppLoading, setIsAppLoading] = useState<boolean>(true);
   return (
     <CurrentAppContext.Provider
       value={{
         isAuthenticated,
-        user,
         setIsAuthenticated,
+        user,
         setUser,
+        isAppLoading,
+        setIsAppLoading,
       }}
     >
       {props.children}
